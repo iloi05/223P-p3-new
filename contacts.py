610107eedf9134d374a306c805f54fbb48c0b5ba
root <root@DESKTOP-8969P1M.localdomain>
2025-09-23 12:17:06 -0700

    finished both files, need to add edge cases and error detections and fix forloop in print contacts

--- contacts.py
+++ contacts.py
@@ -3,34 +3,47 @@
 # Purpose of file: Create contact list functions that will be used in main file
 
 def print_contact(contact_dict):
+    """Prints contact list"""
     if not contact_dict:
         print("The list is empty, put a name in first")
     else:
-        print("")
+        print("================== CONTACT LIST ==================")
+        print("Last Name           First Name           Phone    ")
+        print("==================  ===================  =========")
+        for last, info in contact_dict.items():
+            first = info.get("first", "N/A")
+            phone = info.get("phone", "N/A")
+            print(f"{last:<20}{first:<20}{phone:<10}")
+           
 
 def add_contact(contact_dict, /, *, id, first_name, last_name):
+    """Adds contact to dictionary"""
     if id in contact_dict:
         return "error"
     contact_dict[id] = [first_name, last_name]
     return contact_dict[id]
 
 def modify_contact(contact_dict, *, id, first_name, last_name):
+    """Gets contact and modifies it"""
     if id not in contact_dict:
         return "error"
     contact_dict[id] = [first_name, last_name]
     return contact_dict[id]
 
 def delete_contact(contact_dict, *, id):
+    """Deletes contact from dictionary"""
     if id not in contact_dict:
         return "error"
     rem_contact = contact_dict.pop(id)
     return rem_contact
 
 def sort_contact(contact_dict, /):
+    """Sorts contacts by last name"""
     sorts = sorted(contact_dict.items(), key = lambda item: (item[1][1], item[1][0]))
     return sorts
 
 def find_contact(contact_dict, /, *, find):
+    """Finds contacts by last name"""
     empty_contact = {}
     if isinstance(find, (int, str)) and find in contact_dict:
         empty_contact[find] = contact_dict[find]
--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 from contacts import *
 
 contacts = {}
+choices = ["1", "2", "3", "4", "5", "6"]
 
 while True:
     print("     *** EMPLOYEE CONTACT MAIN MENU")
@@ -16,20 +17,28 @@ while True:
     print("5. Find contact")
     print("6. Exit program")
     print()
-    prompt = input("Enter menuc choice: ")
+    prompt = input("Enter menu choice: ")
     if prompt == "1":
         num = input("Enter phone number: ")
         first = input("Enter first name: ")
         last = input("Enter last name: ")
-        add_contact(contacts, id, first_name  = first, last_name = last)
+        add_contact(contacts, id = num, first_name  = first, last_name = last)
     elif prompt == "2":
         num = input("Enter phone number: ")
         new_first = input("Enter first name: ")
         new_last = input("Enter last name: ")
-        modify_contact(contacts, id, first_name = new_first, last_name = new_last)
-        print("Modified: {new_first}{new_last}")
+        modify_contact(contacts, id = num, first_name = new_first, last_name = new_last)
+        print("Modified: {} {}".format(new_first, new_last))
     elif prompt == "3":
         num = input("Enter phone number: ")
-        delete_contact(contacts, id)
+        delete_contact(contacts, id = num)
     elif prompt == "4":
-        
+        print_contact( sort_contact(contacts))
+    elif prompt == "5":
+        search = input("Enter search string: ")
+        find_contact(contacts, find = search)
+    elif prompt == 6:
+        break
+    if prompt not in choices:
+        print("You've chosen an invalid option try again")
+        continue
